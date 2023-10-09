92625bccdf79eac7069782bafe03f9d6c88034eb
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-09 23:47:32 +0530

    Add: Gallery page updated

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { CldUploadButton } from "next-cloudinary";
+import { UploadResult } from "../page";
+
 export default function GalleryPage() {
-  return <h2>Gallery</h2>;
+  return (
+    <section>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-bold">Gallery</h1>
+        <CldUploadButton onUpload={(result: UploadResult) => {}} />
+      </div>
+      <h1>Gallery</h1>
+    </section>
+  );
 }
