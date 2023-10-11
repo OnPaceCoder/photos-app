f59b8ec4010b12f1acdfce42e39fd1c46d4934ee
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-11 21:12:14 +0530

    Add: serveractions created

---
+++ src/app/gallery/actions.tsx
@@ -0,0 +1,12 @@
+"use server";
+import cloudinary from "cloudinary";
+import { revalidatePath } from "next/cache";
+
+export async function setAsFavouriteAction(publicId: string) {
+  await cloudinary.v2.uploader.add_tag("favourite", [publicId]);
+  revalidatePath("/gallery");
+}
+export async function removeAsFavouriteAction(publicId: string) {
+  await cloudinary.v2.uploader.remove_tag("favourite", [publicId]);
+  revalidatePath("/favourite");
+}
