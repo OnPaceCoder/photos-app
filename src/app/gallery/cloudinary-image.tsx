138ca41f3ceab9fbf5bfabb7a06e2477678463ff
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-11 21:10:24 +0530

    Add: Server actions added

--- src/app/gallery/cloudinary-image.tsx
+++ src/app/gallery/cloudinary-image.tsx
@@ -1,7 +1,37 @@
 "use client";
 
+import Heart from "@/components/icons/heart";
 import { CldImage } from "next-cloudinary";
+import cloudinary from "cloudinary";
+import { removeAsFavouriteAction, setAsFavouriteAction } from "./actions";
+import { useTransition } from "react";
+import { FullHeart } from "@/components/icons/fullHeart";
+export function CloudinaryImage(props: any & { public_id: string }) {
+  const [transition, startTransition] = useTransition();
+  const isFavourible = props.imageData.tags.includes("favourite");
 
-export function CloudinaryImage(props: any) {
-  return <CldImage {...props} />;
+  return (
+    <div className="relative">
+      <CldImage {...props} />
+      {isFavourible ? (
+        <FullHeart
+          className={`absolute top-2 right-2 hover:text-red-500 cursor-pointer`}
+          onClick={() => {
+            startTransition(() => {
+              removeAsFavouriteAction(props.public_id);
+            });
+          }}
+        />
+      ) : (
+        <Heart
+          className={`absolute top-2 right-2 hover:text-red-500 cursor-pointer`}
+          onClick={() => {
+            startTransition(() => {
+              setAsFavouriteAction(props.public_id);
+            });
+          }}
+        />
+      )}
+    </div>
+  );
 }
